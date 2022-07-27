eebe42fa3adda955d41039cc20be54bf006a42d7
Your Name <asrinziyahocaoglu19@gmail.com>
2022-07-27 16:12:36 +0300

    End Of Section 9

--- client/src/app/_models/user.ts
+++ client/src/app/_models/user.ts
@@ -3,3 +3,30 @@ export interface User {
     token: string;
     
 }
+
+// //TypeScript
+// let data: number | string = 42; // hem string hemde number olabilir
+
+// data = "10";
+
+// interface Car{
+//     color:string;
+//     model:string;
+//     topSpeed?: number; // nullable
+// }
+
+// const car1: Car = {
+//     color : 'blue',
+//     model: 'BMW',
+
+// }
+
+// const car2 : Car = {
+//     color:'red',
+//     model:'Mercedes',
+//     topSpeed:100
+// }
+
+// const multiply = (x: number,y:number)=> {
+//      x*y;
+// }
