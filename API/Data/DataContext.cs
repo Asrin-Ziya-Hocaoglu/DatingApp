1e785802d873ad658032de5062b1478ef1b3bd8d
Your Name <asrinziyahocaoglu19@gmail.com>
2022-09-01 15:52:53 +0300

    End Of Section 15

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -15,6 +15,7 @@ namespace API.Data
         }
         public DbSet<AppUser> AppUsers { get; set; }
         public DbSet<UserLike> Likes { get; set; }
+        public DbSet<Message> Messages { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -33,6 +34,16 @@ namespace API.Data
             .WithMany(l=>l.LikedByUsers)
             .HasForeignKey(s=>s.LikedUserId)
             .OnDelete(DeleteBehavior.NoAction);
+
+            builder.Entity<Message>()
+            .HasOne(s=>s.Recipient)
+            .WithMany(l=>l.MessagesReceived)
+            .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Message>()
+            .HasOne(s=>s.Sender)
+            .WithMany(l=>l.MessagesSent)
+            .OnDelete(DeleteBehavior.Restrict); // restrict => for deleting both sides
         }
 
     }
--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,9 +1,13 @@
-// import { SimplePlaceholderMapper } from '@angular/compiler/src/i18n/serializers/serializer';
-import { Component, OnInit } from '@angular/core';
+//import { SimplePlaceholderMapper } from '@angular/compiler/src/i18n/serializers/serializer';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from '@kolkov/ngx-gallery';
+import { TabDirective, TabsetComponent } from 'ngx-bootstrap/tabs';
 import { Member } from 'src/app/_models/member';
+import { Message } from 'src/app/_models/message';
 import { MembersService } from 'src/app/_services/members.service';
+import { MessageService } from 'src/app/_services/message.service';
+
 
 @Component({
   selector: 'app-member-detail',
@@ -11,14 +15,22 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
+  @ViewChild('memberTabs',{static: true}) memberTabs: TabsetComponent;
   member: Member;
   galleryOptions : NgxGalleryOptions[];
   galleryImages : NgxGalleryImage[];
-
-  constructor(private memberService: MembersService, private route: ActivatedRoute) { }
+  activeTab: TabDirective;
+  messages: Message[]=[];
+  
+  constructor(private memberService: MembersService, private route: ActivatedRoute, private messageService: MessageService) { }
 
   ngOnInit(): void {
-    this.loadMember();
+    this.route.data.subscribe(data=>{
+      this.member = data.member;
+    })
+    this.route.queryParams.subscribe(params=>{
+      params.tab ? this.selectTab(params.tab) : this.selectTab(0);
+    });
     this.galleryOptions =
     [{
       width : '500px',
@@ -30,8 +42,15 @@ export class MemberDetailComponent implements OnInit {
 
 
     }]
+    this.galleryImages = this.getImages();
    
   }
+  selectTab(tabId:number)
+  {
+    this.memberTabs.tabs[tabId].active = true;
+  
+  }
+ 
 
   getImages():NgxGalleryImage[] {
     const imageUrls = [];
@@ -46,11 +65,26 @@ export class MemberDetailComponent implements OnInit {
 
   }
 
-  loadMember(){
-    this.memberService.getMember(this.route.snapshot.paramMap.get('username')).subscribe(member=>{
-      this.member = member;
-      this.galleryImages = this.getImages();
-    })
+  loadMessages()
+  {
+    this.messageService.getMessageThread(this.member.username).subscribe(messages=>{
+      this.messages = messages;
+    });
+
   }
+
+
+  
+
+
+  onTabActivated(data:TabDirective)
+  {
+    this.activeTab = data;
+    if(this.activeTab.heading === 'Messages' && this.messages.length === 0)
+    {
+      this.loadMessages();
+    }
+  }
+ 
 
 }
